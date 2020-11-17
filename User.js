6f7b8080b8fda5e60819be27535cb0fd40cf4db6
fowwo <54595444+fowwo@users.noreply.github.com>
2020-11-17 02:57:16 -0600

    Add user class

---
+++ User.js
@@ -0,0 +1,192 @@
+/**
+ * Models a user and his or her guesses.
+ * 
+ * @author fowwo
+ */
+class User {
+
+	constructor(id, username, guesses = { x: 0, a: 0, b: 0 }, wins = { x: 0, a: 0, b: 0 }) {
+		this.id = id;
+		this.username = username;
+		this.guesses = guesses;
+		this.wins = wins;
+	}
+
+	getTotalGuesses() {
+		return this.guesses.x + this.guesses.a + this.guesses.b;
+	}
+	getTotalWins() {
+		return this.wins.x + this.wins.a + this.wins.b;
+	}
+	getNameColor() {
+		let max = Math.max(this.guesses.x, this.guesses.a, this.guesses.b);
+		return `rgb(${Math.round(155 * this.guesses.b / max) + 100},
+		${Math.round(155 * this.guesses.a / max) + 100},
+		${Math.round(155 * this.guesses.x / max) + 100})`;
+	}
+	getAccuracyColor(min = 0, max = 1) {
+		return `rgb(${Math.round(255 * Math.min((2 * max - 2 * this.getTotalWins() / this.getTotalGuesses()) / (max - min), 1))},
+		${Math.round(255 * Math.min((2 * this.getTotalWins() / this.getTotalGuesses() - 2 * min) / (max - min), 1))}
+		,0)`;
+	}
+
+	toLeaderboardRow(rank) {
+		let row = document.createElement("tr");
+		let cell = document.createElement("td");
+		cell.innerHTML = rank;
+		switch (rank) {
+			case 1:
+				cell.style.color = "#fc0";
+				cell.style.textShadow = "0 0 10px #fc0, 0 0 10px #fc0, 0 0 10px #fc0";
+				row.style.backgroundColor = "#fc04";
+				break;
+			case 2:
+				cell.style.color = "#ddd";
+				cell.style.textShadow = "0 0 10px #ddd, 0 0 10px #ddd, 0 0 10px #ddd";
+				row.style.backgroundColor = "#ddd3";
+				break;
+			case 3:
+				cell.style.color = "#d70";
+				cell.style.textShadow = "0 0 10px #d70, 0 0 10px #d70, 0 0 10px #d70";
+				row.style.backgroundColor = "#d702";
+				break;
+		}
+		cell.style.textAlign = "center";
+		row.appendChild(cell);
+
+		cell = document.createElement("td");
+		cell.innerHTML = this.username;
+		cell.classList.add("name-cell");
+		cell.style.color = this.getNameColor();
+		row.appendChild(cell);
+
+		cell = document.createElement("td");
+		cell.innerHTML = this.getTotalWins();
+		cell.style.textAlign = "center";
+		row.appendChild(cell);
+
+		cell = document.createElement("td");
+		cell.innerHTML = this.getTotalGuesses();
+		cell.style.textAlign = "center";
+		row.appendChild(cell);
+
+		cell = document.createElement("td");
+		cell.style.textAlign = "center";
+		if (this.getTotalGuesses() === 0) {
+			cell.innerHTML = "------";
+			cell.style.color = "#cdf5";
+		} else {
+			let rgb = this.getAccuracyColor();
+			cell.style.color = rgb;
+			cell.style.textShadow = `0 0 10px ${rgb}`;
+			cell.innerHTML = (100 * this.getTotalWins() / this.getTotalGuesses()).toFixed(2) + "%";
+		}
+		row.appendChild(cell);
+
+		cell = document.createElement("td");
+		cell.innerHTML = this.guesses.x;
+		cell.style.textAlign = "center";
+		cell.style.color = "var(--light-blue)";
+		row.appendChild(cell);
+		
+		cell = document.createElement("td");
+		cell.innerHTML = this.guesses.a;
+		cell.style.textAlign = "center";
+		cell.style.color = "var(--light-green)";
+		row.appendChild(cell);
+
+		cell = document.createElement("td");
+		cell.innerHTML = this.guesses.b;
+		cell.style.textAlign = "center";
+		cell.style.color = "var(--light-red)";
+		row.appendChild(cell);
+
+		return row;
+	}
+
+	/**
+	 * Compares users by percentage correct.
+	 * 
+	 * @param a The user being compared from.
+	 * @param b The user being compared to.
+	 * @returns -1, 0, or 1.
+	 */
+	static compareByPercentage(a, b) {
+		let x = 0;
+		let y = 0;
+		if (a.guesses.x + a.guesses.a + a.guesses.b > 0) x = (a.wins.x + a.wins.a + a.wins.b) / (a.guesses.x + a.guesses.a + a.guesses.b);
+		if (b.guesses.x + b.guesses.a + b.guesses.b > 0) y = (b.wins.x + b.wins.a + b.wins.b) / (b.guesses.x + b.guesses.a + b.guesses.b);
+		return x < y ? 1 : x === y ? User.compareByTotal(a, b) : -1;
+	}
+
+	/**
+	 * Compares users by username.
+	 * 
+	 * @param a The user being compared from.
+	 * @param b The user being compared to.
+	 * @returns -1, 0, or 1.
+	 */
+	static compareByName(a, b) {
+		return a.username < b.username ? -1 : a.username === b.username ? 0 : 1;
+	}
+
+	/**
+	 * Compares users by number of wins.
+	 * 
+	 * @param a The user being compared from.
+	 * @param b The user being compared to.
+	 * @returns -1, 0, or 1.
+	 */
+	static compareByWins(a, b) {
+		let x = a.wins.x + a.wins.a + a.wins.b;
+		let y = b.wins.x + b.wins.a + b.wins.b;
+		return x > y ? -1 : x === y ? 0 : 1;
+	}
+
+	/**
+	 * Compares users by total number of guesses.
+	 * 
+	 * @param a The user being compared from.
+	 * @param b The user being compared to.
+	 * @returns -1, 0, or 1.
+	 */
+	static compareByTotal(a, b) {
+		let x = a.guesses.x + a.guesses.a + a.guesses.b;
+		let y = b.guesses.x + b.guesses.a + b.guesses.b;
+		return x > y ? -1 : x === y ? 0 : 1;
+	}
+
+	/**
+	 * Compares users by total number of 'X' guesses.
+	 * 
+	 * @param a The user being compared from.
+	 * @param b The user being compared to.
+	 * @returns -1, 0, or 1.
+	 */
+	static compareByTotalX(a, b) {
+		return a.guesses.x > b.guesses.x ? -1 : a.guesses.x === b.guesses.x ? 0 : 1;
+	}
+
+	/**
+	 * Compares users by total number of 'A' guesses.
+	 * 
+	 * @param a The user being compared from.
+	 * @param b The user being compared to.
+	 * @returns -1, 0, or 1.
+	 */
+	static compareByTotalA(a, b) {
+		return a.guesses.a > b.guesses.a ? -1 : a.guesses.a === b.guesses.a ? 0 : 1;
+	}
+
+	/**
+	 * Compares users by total number of 'B' guesses.
+	 * 
+	 * @param a The user being compared from.
+	 * @param b The user being compared to.
+	 * @returns -1, 0, or 1.
+	 */
+	static compareByTotalB(a, b) {
+		return a.guesses.b > b.guesses.b ? -1 : a.guesses.b === b.guesses.b ? 0 : 1;
+	}
+
+}
--- index.js
+++ index.js
@@ -22,7 +22,7 @@ fetch("data.json")
 		var accuracy = 0;
 		for (let [key, value] of Object.entries(data)) {
 			if (key == 'stat') continue;
-			scores.push(value);
+			scores.push(new User(key, value.username, value.guesses, value.wins));
 			guesses.x += value.guesses.x;
 			guesses.a += value.guesses.a;
 			guesses.b += value.guesses.b;
@@ -61,7 +61,7 @@ function sort(list, comparator, reverse = false, filter = false) {
 
 	if (filter) list = list.filter((a) => { return a.guesses.x + a.guesses.a + a.guesses.b >= 10; });
 
-	list.sort((a, b) => (comparator(a, b) === 0 ? compareByName(a, b) : comparator(a, b)) * (reverse ? -1 : 1));
+	list.sort((a, b) => (comparator(a, b) === 0 ? User.compareByName(a, b) : comparator(a, b)) * (reverse ? -1 : 1));
 
 	const table = document.getElementById("leaderboard");
 
@@ -69,167 +69,13 @@ function sort(list, comparator, reverse = false, filter = false) {
 		table.deleteRow(-1);
 	}
 
-	var row;
-	var cell;
-
 	var rank = 1;
 	for (var i = 0; i < list.length; i++) {
-		let a = list[i];
-		let totalWins = a.wins.x + a.wins.a + a.wins.b;
-		let totalGuesses = a.guesses.x + a.guesses.a + a.guesses.b;
-
-		row = document.createElement("tr");
-
-		cell = document.createElement("td");
+		let user = list[i];
 		if (i > 0 && comparator(list[i], list[i - 1]) !== 0) {
 			rank = i + 1;
 		}
-		cell.innerHTML = rank;
-		if (rank === 1) {
-			cell.style = "text-align: center; color: #fc0; text-shadow: 0 0 10px #fc0, 0 0 10px #fc0, 0 0 10px #fc0;";
-			row.style = "background-color: #fc04;";
-		} else if (rank === 2) {
-			cell.style = "text-align: center; color: #ddd; text-shadow: 0 0 10px #ddd, 0 0 10px #ddd, 0 0 10px #ddd;";
-			row.style = "background-color: #ddd3;";
-		} else if (rank === 3) {
-			cell.style = "text-align: center; color: #d70; text-shadow: 0 0 10px #d70, 0 0 10px #d70, 0 0 10px #d70;";
-			row.style = "background-color: #d702;";
-		} else {
-			cell.style = "text-align: center;";
-		}
-		row.appendChild(cell);
-
-		cell = document.createElement("td");
-		cell.innerHTML = a.username;
-		cell.classList.add("name-cell");
-		let maxGuess = Math.max(a.guesses.x, a.guesses.a, a.guesses.b);
-		cell.style.color = `rgb(${Math.round(155 * a.guesses.b / maxGuess) + 100}, ${Math.round(155 * a.guesses.a / maxGuess) + 100}, ${Math.round(155 * a.guesses.x / maxGuess) + 100})`;
-		row.appendChild(cell);
-
-		cell = document.createElement("td");
-		cell.innerHTML = totalWins;
-		cell.style = "text-align: center;";
-		row.appendChild(cell);
-
-		cell = document.createElement("td");
-		cell.innerHTML = totalGuesses;
-		cell.style = "text-align: center;";
-		row.appendChild(cell);
-
-		cell = document.createElement("td");
-		if (totalGuesses === 0) {
-			cell.innerHTML = "------";
-			cell.style = "text-align: center;color: #cdf5;";
-		} else {
-			let rgb = `rgb(${Math.round(255 * Math.min((2 - 2 * totalWins / totalGuesses), 1))},${Math.round(255 * Math.min((2 * totalWins / totalGuesses), 1))},0)`;
-			cell.style = `text-align: center;color: ${rgb};text-shadow: 0 0 10px ${rgb};`;
-			cell.innerHTML = (100 * totalWins / totalGuesses).toFixed(2) + "%";
-		}
-		row.appendChild(cell);
-
-		cell = document.createElement("td");
-		cell.innerHTML = a.guesses.x;
-		cell.style = "text-align: center; color: var(--light-blue);";
-		row.appendChild(cell);
-		
-		cell = document.createElement("td");
-		cell.innerHTML = a.guesses.a;
-		cell.style = "text-align: center; color: var(--light-green);";
-		row.appendChild(cell);
-
-		cell = document.createElement("td");
-		cell.innerHTML = a.guesses.b;
-		cell.style = "text-align: center; color: var(--light-red);";
-		row.appendChild(cell);
-
-		table.appendChild(row);
+		table.appendChild(user.toLeaderboardRow(rank));
 	}
 
-	table.appendChild(row);
-
-}
-
-/**
- * Compares scores by percentage correct.
- * 
- * @param a The score being compared from.
- * @param b The score being compared to.
- * @returns -1, 0, or 1.
- */
-function compareByPercentage(a, b) {
-	let x = 0;
-	let y = 0;
-	if (a.guesses.x + a.guesses.a + a.guesses.b > 0) x = (a.wins.x + a.wins.a + a.wins.b) / (a.guesses.x + a.guesses.a + a.guesses.b);
-	if (b.guesses.x + b.guesses.a + b.guesses.b > 0) y = (b.wins.x + b.wins.a + b.wins.b) / (b.guesses.x + b.guesses.a + b.guesses.b);
-	return x < y ? 1 : x === y ? compareByTotal(a, b) : -1;
-}
-
-/**
- * Compares scores by username.
- * 
- * @param a The score being compared from.
- * @param b The score being compared to.
- * @returns -1, 0, or 1.
- */
-function compareByName(a, b) {
-	return a.username < b.username ? -1 : a.username === b.username ? 0 : 1;
-}
-
-/**
- * Compares scores by number of wins.
- * 
- * @param a The score being compared from.
- * @param b The score being compared to.
- * @returns -1, 0, or 1.
- */
-function compareByWins(a, b) {
-	var x = a.wins.x + a.wins.a + a.wins.b;
-	var y = b.wins.x + b.wins.a + b.wins.b;
- 	return x > y ? -1 : x === y ? 0 : 1;
-}
-
-/**
- * Compares scores by total number of guesses.
- * 
- * @param a The score being compared from.
- * @param b The score being compared to.
- * @returns -1, 0, or 1.
- */
-function compareByTotal(a, b) {
-	var x = a.guesses.x + a.guesses.a + a.guesses.b;
-	var y = b.guesses.x + b.guesses.a + b.guesses.b;
-	return x > y ? -1 : x === y ? 0 : 1;
-}
-
-/**
- * Compares scores by total number of 'X' guesses.
- * 
- * @param a The score being compared from.
- * @param b The score being compared to.
- * @returns -1, 0, or 1.
- */
-function compareByTotalX(a, b) {
-	return a.guesses.x > b.guesses.x ? -1 : a.guesses.x === b.guesses.x ? 0 : 1;
-}
-
-/**
- * Compares scores by total number of 'A' guesses.
- * 
- * @param a The score being compared from.
- * @param b The score being compared to.
- * @returns -1, 0, or 1.
- */
-function compareByTotalA(a, b) {
-	return a.guesses.a > b.guesses.a ? -1 : a.guesses.a === b.guesses.a ? 0 : 1;
-}
-
-/**
- * Compares scores by total number of 'B' guesses.
- * 
- * @param a The score being compared from.
- * @param b The score being compared to.
- * @returns -1, 0, or 1.
- */
-function compareByTotalB(a, b) {
-	return a.guesses.b > b.guesses.b ? -1 : a.guesses.b === b.guesses.b ? 0 : 1;
 }
