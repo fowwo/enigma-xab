c1f8944fb9c7f47a2a19fb3475db68a1d7ce24fc
fowwo <54595444+fowwo@users.noreply.github.com>
2021-05-28 19:04:56 -0500

    Add leaderboard

---
+++ User.js
@@ -0,0 +1,203 @@
+class User {
+
+	constructor(id, username, guesses = { x: 0, a: 0, b: 0 }, wins = { x: 0, a: 0, b: 0 }) {
+		this.id = id;
+		this.username = username;
+		this.guesses = guesses;
+		this.wins = wins;
+		this.badges = { gold: 0, silver: 0, bronze: 0 };
+	}
+
+	getTotalGuesses() {
+		return this.guesses.x + this.guesses.a + this.guesses.b;
+	}
+	getTotalWins() {
+		return this.wins.x + this.wins.a + this.wins.b;
+	}
+	getNameColor() {
+		let max = Math.max(this.guesses.x, this.guesses.a, this.guesses.b);
+		return `rgb(${Math.round(155 * this.guesses.b / max) + 100},
+		${Math.round(155 * this.guesses.a / max) + 100},
+		${Math.round(155 * this.guesses.x / max) + 100})`;
+	}
+	getAccuracyColor(min = 0, max = 1) {
+		return `rgb(${Math.round(255 * Math.min((2 * max - 2 * this.getTotalWins() / this.getTotalGuesses()) / (max - min), 1))},
+		${Math.round(255 * Math.min((2 * this.getTotalWins() / this.getTotalGuesses() - 2 * min) / (max - min), 1))}
+		,0)`;
+	}
+
+	toLeaderboardRow(rank) {
+		let row = document.createElement("tr");
+		let cell = document.createElement("td");
+		cell.innerHTML = rank;
+		switch (rank) {
+			case 1:
+				cell.style.color = "var(--gold)";
+				cell.style.textShadow = "0 0 10px var(--gold), 0 0 10px var(--gold), 0 0 10px var(--gold)";
+				row.style.backgroundColor = "var(--background-gold)";
+				break;
+			case 2:
+				cell.style.color = "var(--silver)";
+				cell.style.textShadow = "0 0 10px var(--silver), 0 0 10px var(--silver), 0 0 10px var(--silver)";
+				row.style.backgroundColor = "var(--background-silver)";
+				break;
+			case 3:
+				cell.style.color = "var(--bronze)";
+				cell.style.textShadow = "0 0 10px var(--bronze), 0 0 10px var(--bronze), 0 0 10px var(--bronze)";
+				row.style.backgroundColor = "var(--background-bronze)";
+				break;
+		}
+		cell.style.textAlign = "center";
+		row.appendChild(cell);
+
+		cell = document.createElement("td");
+		let badges = document.createElement("span");
+		for (var i = 0; i < this.badges.gold; i++) {
+			let badge = document.createElement("span");
+			badge.innerHTML = "★";
+			badge.classList.add("badge");
+			badge.style.color = "var(--gold)";
+			badge.style.textShadow = "0 0 10px var(--gold), 0 0 5px #000f";
+			badge.style.zIndex = 18 - i;
+			badges.appendChild(badge);
+		}
+		for (var i = 0; i < this.badges.silver; i++) {
+			let badge = document.createElement("span");
+			badge.innerHTML = "★";
+			badge.classList.add("badge");
+			badge.style.color = "var(--silver)";
+			badge.style.textShadow = "0 0 10px var(--silver), 0 0 5px #000f";
+			badge.style.zIndex = 12 - i;
+			badges.appendChild(badge);
+		}
+		for (var i = 0; i < this.badges.bronze; i++) {
+			let badge = document.createElement("span");
+			badge.innerHTML = "★";
+			badge.classList.add("badge");
+			badge.style.color = "var(--bronze)";
+			badge.style.textShadow = "0 0 10px var(--bronze), 0 0 5px #000f";
+			badge.style.zIndex = 6 - i;
+			badges.appendChild(badge);
+		}
+		if (badges.childElementCount !== 0) {
+			badges.classList.add("badges");
+			cell.appendChild(badges);
+		}
+		cell.innerHTML += this.username;
+		cell.style.color = this.getNameColor();
+		row.appendChild(cell);
+
+		cell = document.createElement("td");
+		cell.innerHTML = this.getTotalWins();
+		row.appendChild(cell);
+
+		cell = document.createElement("td");
+		cell.innerHTML = this.getTotalGuesses();
+		row.appendChild(cell);
+
+		cell = document.createElement("td");
+		cell.innerHTML = (100 * this.getTotalWins() / this.getTotalGuesses()).toFixed(2) + "%";
+		cell.style.color = this.getAccuracyColor();
+		row.appendChild(cell);
+
+		cell = document.createElement("td");
+		cell.innerHTML = this.guesses.x;
+		row.appendChild(cell);
+		
+		cell = document.createElement("td");
+		cell.innerHTML = this.guesses.a;
+		row.appendChild(cell);
+
+		cell = document.createElement("td");
+		cell.innerHTML = this.guesses.b;
+		row.appendChild(cell);
+
+		return row;
+	}
+
+	/**
+	 * Compares users by percentage correct.
+	 * 
+	 * @param a The user being compared from.
+	 * @param b The user being compared to.
+	 * @returns -1, 0, or 1.
+	 */
+	static compareByPercentage(a, b) {
+		let x = 0;
+		let y = 0;
+		if (a.guesses.x + a.guesses.a + a.guesses.b > 0) x = (a.wins.x + a.wins.a + a.wins.b) / (a.guesses.x + a.guesses.a + a.guesses.b);
+		if (b.guesses.x + b.guesses.a + b.guesses.b > 0) y = (b.wins.x + b.wins.a + b.wins.b) / (b.guesses.x + b.guesses.a + b.guesses.b);
+		return x < y ? 1 : x === y ? User.compareByTotal(a, b) : -1;
+	}
+
+	/**
+	 * Compares users by username.
+	 * 
+	 * @param a The user being compared from.
+	 * @param b The user being compared to.
+	 * @returns -1, 0, or 1.
+	 */
+	static compareByName(a, b) {
+		return a.username < b.username ? -1 : a.username === b.username ? 0 : 1;
+	}
+
+	/**
+	 * Compares users by number of wins.
+	 * 
+	 * @param a The user being compared from.
+	 * @param b The user being compared to.
+	 * @returns -1, 0, or 1.
+	 */
+	static compareByWins(a, b) {
+		let x = a.wins.x + a.wins.a + a.wins.b;
+		let y = b.wins.x + b.wins.a + b.wins.b;
+		return x > y ? -1 : x === y ? 0 : 1;
+	}
+
+	/**
+	 * Compares users by total number of guesses.
+	 * 
+	 * @param a The user being compared from.
+	 * @param b The user being compared to.
+	 * @returns -1, 0, or 1.
+	 */
+	static compareByTotal(a, b) {
+		let x = a.guesses.x + a.guesses.a + a.guesses.b;
+		let y = b.guesses.x + b.guesses.a + b.guesses.b;
+		return x > y ? -1 : x === y ? 0 : 1;
+	}
+
+	/**
+	 * Compares users by total number of 'X' guesses.
+	 * 
+	 * @param a The user being compared from.
+	 * @param b The user being compared to.
+	 * @returns -1, 0, or 1.
+	 */
+	static compareByTotalX(a, b) {
+		return a.guesses.x > b.guesses.x ? -1 : a.guesses.x === b.guesses.x ? 0 : 1;
+	}
+
+	/**
+	 * Compares users by total number of 'A' guesses.
+	 * 
+	 * @param a The user being compared from.
+	 * @param b The user being compared to.
+	 * @returns -1, 0, or 1.
+	 */
+	static compareByTotalA(a, b) {
+		return a.guesses.a > b.guesses.a ? -1 : a.guesses.a === b.guesses.a ? 0 : 1;
+	}
+
+	/**
+	 * Compares users by total number of 'B' guesses.
+	 * 
+	 * @param a The user being compared from.
+	 * @param b The user being compared to.
+	 * @returns -1, 0, or 1.
+	 */
+	static compareByTotalB(a, b) {
+		return a.guesses.b > b.guesses.b ? -1 : a.guesses.b === b.guesses.b ? 0 : 1;
+	}
+
+}
--- index.js
+++ index.js
@@ -60,3 +60,120 @@ window.addEventListener("resize", function(){
 		if (tail.style.maxHeight) tail.style.maxHeight = `${tail.scrollHeight}px`;
 	}
 });
+
+// Fetch user data
+var scores = {};
+var activeLeaderboard = document.getElementById("leaderboard-placeholder");
+fetch("data.json").then(r => r.json()).then(data => {
+	let users = [];
+	var accuracy = 0;
+	var total = {
+		guesses: {
+			x: 0,
+			a: 0,
+			b: 0
+		},
+		wins: {
+			x: 0,
+			a: 0,
+			b: 0
+		}
+	}
+	const stat = data.stat;
+	delete data.stat;
+
+	for (let [key, value] of Object.entries(data)) {
+		users.push(new User(key, value.username, value.guesses, value.wins));
+		total.guesses.x += value.guesses.x;
+		total.guesses.a += value.guesses.a;
+		total.guesses.b += value.guesses.b;
+		total.wins.x += value.wins.x;
+		total.wins.a += value.wins.a;
+		total.wins.b += value.wins.b;
+		accuracy += (value.wins.x + value.wins.a + value.wins.b) / (value.guesses.x + value.guesses.a + value.guesses.b);
+	}
+
+	function awardBadges(list, comparator) {
+		if (list.length != 0) list[0].badges.gold++;
+		let rank = 1;
+		for (var i = 1; i < list.length; i++) {
+			let user = list[i];
+			if (comparator(list[i], list[i - 1]) !== 0) {
+				rank = i + 1;
+			}
+			switch (rank) {
+				case 1:
+					user.badges.gold++;
+					break;
+				case 2:
+					user.badges.silver++;
+					break;
+				case 3:
+					user.badges.bronze++;
+					break;
+				default:
+					return;
+			}
+		}
+	}
+	
+	function toLeaderboard(list, comparator, id) {
+		let table = document.createElement("table");
+		let tbody = document.createElement("tbody");
+		let tr = document.createElement("tr");
+		for (var a of ["#", "Name"]) {
+			let th = document.createElement("th");
+			th.innerHTML = a;
+			tr.appendChild(th);
+		}
+		for (var a of ["Wins", "Total", "Accuracy", "X", "A", "B"]) {
+			let th = document.createElement("th");
+			th.innerHTML = a;
+			th.classList.add("clickable");
+			th.onclick = () => {
+				activeLeaderboard.classList.remove("active");
+				activeLeaderboard = document.getElementById(`leaderboard-${th.innerHTML.toLowerCase()}`);
+				activeLeaderboard.classList.add("active");
+			};
+			tr.appendChild(th);
+		}
+		tbody.appendChild(tr);
+
+		var rank = 1;
+		for (var i = 0; i < list.length; i++) {
+			if (i > 0 && comparator(list[i], list[i - 1]) !== 0) rank = i + 1;
+			tbody.appendChild(list[i].toLeaderboardRow(rank));
+		}
+
+		table.id = id;
+		table.classList.add("leaderboard");
+		table.appendChild(tbody);
+		container.appendChild(table);
+	}
+
+	// Sort users
+	scores.wins = [...users].sort((a, b) => User.compareByWins(a, b) === 0 ? User.compareByName(a, b) : User.compareByWins(a, b));
+	scores.total = [...users].sort((a, b) => User.compareByTotal(a, b) === 0 ? User.compareByName(a, b) : User.compareByTotal(a, b));
+	scores.accuracy = [...users].filter(x => x.getTotalGuesses() >= 10).sort((a, b) => User.compareByPercentage(a, b) === 0 ? User.compareByName(a, b) : User.compareByPercentage(a, b));
+	scores.x = [...users].sort((a, b) => User.compareByTotalX(a, b) === 0 ? User.compareByName(a, b) : User.compareByTotalX(a, b));
+	scores.a = [...users].sort((a, b) => User.compareByTotalA(a, b) === 0 ? User.compareByName(a, b) : User.compareByTotalA(a, b));
+	scores.b = [...users].sort((a, b) => User.compareByTotalB(a, b) === 0 ? User.compareByName(a, b) : User.compareByTotalB(a, b));
+
+	// Award badges
+	awardBadges(scores.wins, User.compareByWins);
+	awardBadges(scores.total, User.compareByTotal);
+	awardBadges(scores.accuracy, User.compareByPercentage);
+	awardBadges(scores.x, User.compareByTotalX);
+	awardBadges(scores.a, User.compareByTotalA);
+	awardBadges(scores.b, User.compareByTotalB);
+
+	// Create leaderboards
+	toLeaderboard(scores.wins, User.compareByWins, "leaderboard-wins");
+	toLeaderboard(scores.total, User.compareByTotal, "leaderboard-total");
+	toLeaderboard(scores.accuracy, User.compareByPercentage, "leaderboard-accuracy");
+	toLeaderboard(scores.x, User.compareByTotalX, "leaderboard-x");
+	toLeaderboard(scores.a, User.compareByTotalA, "leaderboard-a");
+	toLeaderboard(scores.b, User.compareByTotalB, "leaderboard-b");
+
+	users.sort((a, b) => User.compareByName(a, b));
+});
